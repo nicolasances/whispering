540f1e8b934fa7d2aae5c9108812855a0b8334ac
nicolasances <nicolas.matteazzi@gmail.com>
2026-01-12 13:04:20 +0100

    Basic usage of Whisper

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, UploadFile, File
+import os
+import shutil
+from whispercpp import Whisper
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    expose_headers=["Content-Disposition"],
+)
+
+w = Whisper.from_pretrained('tiny')
+UPLOAD_DIR="/tmp"
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+    
+@app.post('/transcribe')
+async def transcriptions(file: UploadFile = File(...)):
+    filename = file.filename
+    fileobj = file.file
+    upload_name = os.path.join(UPLOAD_DIR, filename)
+    upload_file = open(upload_name, 'wb+')
+    shutil.copyfileobj(fileobj, upload_file)
+    upload_file.close()
+    
+    result = w.transcribe(upload_name)
+    text = w.extract_text(result)
+    
+    return text
